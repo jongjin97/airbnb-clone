ed174cfb245f5c9e6e8d74675756b43cd7102611
jongjin97 <kakao1302@naver.com>
2023-11-03 18:00:07 +0900

    Revert

--- backend/src/main/java/com/example/airbnb/dto/ChatMessageDto.java
+++ backend/src/main/java/com/example/airbnb/dto/ChatMessageDto.java
@@ -1,8 +1,11 @@
 package com.example.airbnb.dto;
 
+<<<<<<< HEAD
 import com.example.airbnb.document.ChatMessage;
 import com.example.airbnb.document.User;
 import io.swagger.annotations.ApiModelProperty;
+=======
+>>>>>>> parent of c842053 (ChatRoom 생성, 조회, ChatMessage 저장)
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,17 +15,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 @Builder
-public class ChatMessageDto{
-    @ApiModelProperty (value = "채팅방 ID")
+public class ChatMessageDto {
     private String chatRoomId;
     @ApiModelProperty (value = "메시지 보낸 사람 ID")
     private ResponseUser senderId;
     @ApiModelProperty (value = "메시지 내용")
     private String content;
-
-    public ChatMessageDto(ChatMessage chatMessage){
-        this.chatRoomId = chatMessage.getChatRoomId();
-        this.senderId = new ResponseUser(chatMessage.getSender());
-        this.content = chatMessage.getContent();
-    }
 }
