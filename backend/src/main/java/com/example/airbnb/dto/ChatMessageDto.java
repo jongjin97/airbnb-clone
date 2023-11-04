4322ae73c9e7b8601f85afe16b5f463db3f4fa5b
jongjin97 <kakao1302@naver.com>
2023-11-04 19:19:53 +0900

    2차 완

--- backend/src/main/java/com/example/airbnb/dto/ChatMessageDto.java
+++ backend/src/main/java/com/example/airbnb/dto/ChatMessageDto.java
@@ -2,8 +2,6 @@ package com.example.airbnb.dto;
 
 import com.example.airbnb.document.ChatMessage;
 import com.example.airbnb.document.User;
-
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,14 +12,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 public class ChatMessageDto{
-    //@ApiModelProperty (value = "채팅방 ID")
     private String chatRoomId;
-    //@ApiModelProperty (value = "메시지 보낸 사람 ID")
     private ResponseUser senderId;
-    //@ApiModelProperty (value = "메시지 내용")
     private String content;
 
-    public ChatMessageDto(ChatMessage chatMessage) {
+    public ChatMessageDto(ChatMessage chatMessage){
         this.chatRoomId = chatMessage.getChatRoomId();
         this.senderId = new ResponseUser(chatMessage.getSender());
         this.content = chatMessage.getContent();
